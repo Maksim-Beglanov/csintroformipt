5d885f6e21ab2bc13e798e0ab8210ca45aa19641
Maxim Beglyanov <maksimbeglanov@gmail.com>
2026-09-07 16:21:03 +0300

    Add closing file after working with it

--- 1-term/1-wk/file-writing.cpp
+++ 1-term/1-wk/file-writing.cpp
@@ -3,9 +3,12 @@
 
 int main(int argc, char* argv[]) {
         std::ofstream output("output.tmp");
+
         int n = std::stoi(argv[1]);
         for (int i = 1; i <= n; i++)
                 output << i << " ";
+
+        output.close()
 
         return 0;
 }
