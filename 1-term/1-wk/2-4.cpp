a72af47b50ac3894ee42abfa5150763a8006f7b9
Maxim Beglyanov <maksimbeglanov@gmail.com>
2026-09-07 14:33:52 +0300

    Add first 4 task of first week

---
+++ 1-term/1-wk/2-4.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+int main() {
+        // 2 task
+        std::cout << "Hello, world!" << std::endl;
+
+        // 3 task
+        for (int i = 1; i <= 30; i++)
+                std::cout << i << " ";
+        std::cout << std::endl;
+
+        // 4 task
+        // just type ./program > output.tmp
+        
+        return 0;
+}
