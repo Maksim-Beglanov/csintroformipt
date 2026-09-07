f33d9b767856e99936339b348401bf3024a9f5c9
Maxim Beglyanov <maksimbeglanov@gmail.com>
2026-09-07 16:20:00 +0300

    Rename files into more understandable format

--- 1-term/1-wk/file-writing.cpp
+++ 1-term/1-wk/file-writing.cpp
@@ -2,7 +2,6 @@
 #include <string>
 
 int main(int argc, char* argv[]) {
-        // 5 task
         std::ofstream output("output.tmp");
         int n = std::stoi(argv[1]);
         for (int i = 1; i <= n; i++)
