34cd2d1524b7f8b0cc2e568617245d334d2dcd08
Maxim Beglyanov <maksimbeglanov@gmail.com>
2026-09-14 12:57:57 +0300

    Added checking for argument count in week 1 task

--- 1-term/1-wk/file-writing.cpp
+++ 1-term/1-wk/file-writing.cpp
@@ -1,10 +1,15 @@
+#include <iostream>
 #include <fstream>
 #include <string>
 
 int main(int argc, char* argv[]) {
+        if (argc != 2) {
+                std::cout << "Using: ./prog file_name" << std::endl;
+                return 1;
+        }
         std::ofstream output("output.tmp");
 
-        int n = std::stoi(argv[1]);
+        int max_n = std::stoi(argv[1]);
         for (int i = 1; i <= n; i++)
                 output << i << " ";
 
